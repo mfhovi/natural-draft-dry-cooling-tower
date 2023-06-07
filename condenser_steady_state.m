f96f862e8510ee61f5dbdb3edb946e4d8630499a
mfhovi <69019744+mfhovi@users.noreply.github.com>
2023-06-08 02:57:48 +0600

    Add files via upload

---
+++ condenser_steady_state.m
@@ -0,0 +1,54 @@
+clear all
+
+%% condenser 两台凝汽器
+Dc=895.8836111;%kg/s
+
+L1=0.5;%壁厚mm
+mw=49127;% kg/s
+tw=18;
+n=11500*4;
+dic=0.024;
+doc=0.025;
+rhoaw=(1.49343e-3-3.7164e-6*(tw+273.15)+7.09782e-9*(tw+273.15)^2-1.90321e-20*(tw+273.15)^6)^-1;%kg/m3
+vw=mw/(rhoaw*n*pi*dic^2/4);
+K0= 12.87*vw^5 - 145.1*vw^4 + 638.4*vw^3 - 1525*vw^2 + 3007*vw + 714.7;%流速单位为m/s,K0单位为W/m^2*k;
+Fw= 1.151*exp(-((tw-55.95)/75.15)^2) + 0.07026 *exp(-((tw-20.01)/14.15)^2);%水温单位为摄氏度  
+Fm= (0.002362*L1^2 - 0.2093*L1 + 4.056) / (L1 + 3.863);%壁厚单位为mm
+Fc=0.9;
+Kc=0.001*K0*Fw*Fm*Fc;%KJ/(m^2*K*s）
+cpw=4.1868;%KJ/(Kg*K)
+A=24769.5*4;
+L=A/(pi*doc*n);%  
+ts=28.9;%input
+H1 = py.CoolProp.CoolProp.PropsSI('H','T',ts+273.15,'Q',1,'water')*.001;
+H2 = py.CoolProp.CoolProp.PropsSI('H','T',ts+273.15,'Q',0,'water')*.001;
+%H1=refpropm('H','T',ts+273.15,'Q',1,'water')*0.001;%kJ/kg
+%H2=refpropm('H', 'T',ts+273.15,'Q',0, 'water')*0.001;
+Q=Dc*(H1-H2);%流量kg/s，KJ/(Kg)，KJ/(m^2*K*s），
+tw1=Dc*(H1-H2)/(cpw*mw)+tw;
+Tm=(tw1-tw)/log((ts-tw)/(ts-tw1));
+Qh=Kc*A*Tm;%KJ/(m^2*K*s） m^2 
+
+
+while abs((Q-Qh)/Q)>0.000001
+    if ((Q-Qh)/Q)>0
+ts=ts+0.05*abs((Q-Qh)/Q);
+    else
+        ts=ts-0.05*abs((Q-Qh)/Q);
+    end
+H1 = py.CoolProp.CoolProp.PropsSI('H','T',ts+273.15,'Q',1,'water')*.001;
+H2 = py.CoolProp.CoolProp.PropsSI('H','T',ts+273.15,'Q',0,'water')*.001;
+%H1=refpropm('H','T',ts+273.15,'Q',1,'water')*0.001;%
+%H2=refpropm('H', 'T',ts+273.15,'Q',0, 'water')*0.001;
+Q=Dc*(H1-H2);%流量kg/s，KJ/(Kg*K)，KJ/(m^2*K*s），
+tw1=Dc*(H1-H2)/(cpw*mw)+tw;
+
+Tm=(tw1-tw)/log((ts-tw)/(ts-tw1));
+Qh=Kc*A*Tm;%KJ/(m^2*K*s） m^2 
+
+%fprintf("ts=%e\n",ts);
+end 
+pc = py.CoolProp.CoolProp.PropsSI('P', 'T',ts+273.15, 'Q',0, 'water');%Kpa
+%pc=refpropm('P', 'T',ts+273.15, 'Q',0, 'water');%Kpa
+
+
